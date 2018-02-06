eaa48dc3fd17023fdadebe47b190bd8cfb4b459f
rcandido <remycandido@yahoo.fr>
2018-02-06 23:55:45 +0100

    refactor 2

--- app/services/dataServices.js
+++ app/services/dataServices.js
@@ -8,14 +8,13 @@ import {
 (function () {
     'use strict';
     let services = angular.module('dataServices', ['ngResource']);
-
-    services.factory('GetArtDatas', ['$resource', '$q',  '$rootScope', function ($resource, $q, $rootScope) {
+    services.factory('GetArtDatas', ['$resource', '$q', '$rootScope', function ($resource, $q, $rootScope) {
         let artDatas = {};
 
         // Get all events
         artDatas.getEvents = () => {
             let agenda = [];
-           
+
             agenda = $rootScope.cachedAgenda ? $rootScope.cachedAgenda : $resource('/app/assets/data/agenda.json');
             $rootScope.cachedAgenda = agenda;
             return agenda;
@@ -24,21 +23,20 @@ import {
         // Get one event with id 'id'
         artDatas.getEvent = (id) => {
             let deferred = $q.defer();
-            if($rootScope.cachedAgendaById && $rootScope.cachedAgendaById[id]) {
-                deferred.resolve($rootScope.cachedAgendaById);              
+            if ($rootScope.cachedAgendaById && $rootScope.cachedAgendaById[id]) {
+                deferred.resolve($rootScope.cachedAgendaById);
             } else {
-                if(!$rootScope.cachedAgendaById) {
-                    $rootScope.cachedAgendaById = [];                         
-                } 
+                if (!$rootScope.cachedAgendaById) {
+                    $rootScope.cachedAgendaById = [];
+                }
                 let result = false;
                 let BreakException = {};
-                let start= Date.now();
-               
-    
+                let start = Date.now();
+
                 let agenda = [];
                 let AllAvents = artDatas.getEvents();
                 let pEvents = AllAvents.query();
-    
+
                 let i = 0;
                 pEvents.$promise.then(function (data) {
                     try {
@@ -64,83 +62,71 @@ import {
                     deferred.resolve(result);
                 });
             }
-            
+
             return deferred.promise;
         }
-        // Get slide info
+        // Get slide img
         artDatas.getSlide = (sd, $rootScope) => {
             let result = [];
             let slide = [];
-    
+
             let deferred = $q.defer();
-            if(!$rootScope.slides) {
+            if (!$rootScope.slides) {
                 $rootScope.slides = [];
             }
-            // Do nothing and resolve de propise, already serched
-            if($rootScope.slide && $rootScope.slides[sd]){
-                        
-                        console.log('%c cf cache! ', 'background: #123; color: #cada55');
-                        console.log('%c $rootScope.slides[sd] cached', $rootScope.slides[sd]);
-                        deferred.resolve($rootScope.slides[sd]);
-            }
-            // New event (doesn't display)
-            else {
-      
+            // Do nothing and resolve promise, already searched
+            if ($rootScope.slide && $rootScope.slides[sd]) {
+
+                deferred.resolve($rootScope.slides[sd]);
+            }      
+            else { // New event (didn't displayed before)
+
                 let AllAvents = artDatas.getEvents();
                 let pEvents = AllAvents.query();
                 $rootScope.slide = [];
                 let slides = [];
-               
-                let slidesLenght = [];
-                console.log('%c cf db! ', 'background: #222; color: #bada55');
+
                 pEvents.$promise.then(function (data) {
-                   
-                    data.forEach(element => {                                 
+                    data.forEach(element => {
                         result.push({
-                            id: element.id,
-                            number: element.slide
-                        });                
+                            id: element.id, // Id of the event
+                            number: element.slide // Number if photo about this event
+                        });
                     });
-                
-                    if(sd == 0) {
-                        console.log('sd == 0 ');
+                   
+                    if (sd == 0) {  // HomePage
                         let j = 0;
                         result.forEach(si => {
-                       
+                            // Get img scr, id; desription ...
                             for (let i = 0; i < si.number; i++) {
                                 let theSlide = {
-                                    image: 'app/img/' + si.id + '/slider' + i +'.jpg',
-                                    description: 'Image '+ j,
+                                    image: 'app/img/' + si.id + '/slider' + i + '.jpg',
+                                    description: 'Image ' + j,
                                     url: 'desc/' + j,
                                     id: si.id
-                                };                              
+                                };
                                 slide.push(theSlide);
                                 j++;
                             }
                         });
                     } else {
-                            for (let i = 0; i < photoSlideNumber; i++) {
-                                let theSlide = {
-                                    image: 'app/img/' + sd + '/slider' + i +'.jpg',
-                                    description: 'Image '+ i,
-                                    url: 'desc/' + i,
-                                    image: 'app/img/' + sd + '/slider' + i +'.jpg',
-                                    id: sd
-                                };                      
-                                slide.push(theSlide);
-                            }
-                            console.log('sd != 0 ');
-                                          
+                        for (let i = 0; i < photoSlideNumber; i++) {
+                            let theSlide = {
+                                image: 'app/img/' + sd + '/slider' + i + '.jpg',
+                                description: 'Image ' + i,
+                                url: 'desc/' + i,
+                                image: 'app/img/' + sd + '/slider' + i + '.jpg',
+                                id: sd
+                            };
+                            slide.push(theSlide);
+                        }
                     }
-                    console.log('slide => ', slide);
-                    $rootScope.slides[sd] = slide;             
+                    $rootScope.slides[sd] = slide;
                     deferred.resolve(slide);
                 });
             }
-           
             return deferred.promise;
         }
-
         return artDatas;
     }]);
 })();
